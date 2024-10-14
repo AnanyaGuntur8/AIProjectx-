81acb27bf05199218d1a5472fcff636ddeee3c05
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-10-14 12:40:06 -0400

    q2

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -19,13 +19,27 @@
 # Set the given parameters to obtain the specified policies through
 # value iteration.
 
+#discount: care about the future rewards
+# higher discount means higher want for future rewards, lower is immediate
+
+#noise: number of unplanned actions 
+#higher means that the actions may not be planned to more caution 
+#lower means that the actions are more planned
+
+#reward: living reward
+#higher means that the living reward is higher, meaning wants to stay alive
+#lower means that it wants to exit the game quickly 
 def question2a():
     """
       Prefer the close exit (+1), risking the cliff (-10).
     """
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # discouts are more of an average here since the problem doesnt really state it 
+    # noise is, has to be very planned  because riksing the cliff so anywhere from 0.01 - 0.09 choose the intermediate
+    # since close exit then the game is ended quickly 
+    #
+    answerDiscount = 0.4
+    answerNoise = 0.03
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
@@ -33,9 +47,13 @@ def question2b():
     """
       Prefer the close exit (+1), but avoiding the cliff (-10).
     """
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # discouts are more of an average here since the problem doesnt really state it 
+    # noise is, can be slightly unplanned since avoiding the the clift
+    # since close exit then the game is ended quickly 
+    #
+    answerDiscount = 0.5
+    answerNoise = 0.2
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
@@ -43,9 +61,13 @@ def question2c():
     """
       Prefer the distant exit (+10), risking the cliff (-10).
     """
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+     # discouts: prefering the +!0 menaning a need for future rewards 
+    # noise is, has to be very planned  because riksing the cliff so anywhere from 0.01 - 0.09 choose the intermediate
+    # the game is ended quickly 
+    #
+    answerDiscount = 0.9
+    answerNoise = 0.04
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
@@ -53,9 +75,13 @@ def question2d():
     """
       Prefer the distant exit (+10), avoiding the cliff (-10).
     """
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+     # discouts: prefering the +10 menaning a need for future rewards so closer to 1
+    # noise is, has to be somewhat unplanned to avoid the cliff
+    # the game is ended quickly ("exit")
+    #
+    answerDiscount = 0.8
+    answerNoise = 0.3
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
@@ -63,9 +89,13 @@ def question2e():
     """
       Avoid both exits and the cliff (so an episode should never terminate).
     """
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # discouts: prefering the +10 menaning a need for future rewards so closer to 1
+    # noise is, has to be somewhat unplanned to avoid the cliff
+    # the game shuld never terminate so that means that the living rewards is higher 
+    #
+    answerDiscount = 0.9
+    answerNoise = 0.3
+    answerLivingReward = 10
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
